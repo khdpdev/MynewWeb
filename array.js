d71d2f4b935d260ec32324125fd054426220c51f
khdpdev <khdpdeveloper@gmail.com>
2023-09-12 13:14:43 -0400

    Agg archivo JS

---
+++ array.js
@@ -0,0 +1,15 @@
+let arrayNumbers = [12, 28, 189, 1879, 4, 29]
+let maxNumber = 0
+let tamaño = arrayNumbers.length
+
+for (let i = 0; i < tamaño; i++) {
+  if (maxNumber < arrayNumbers[i]) {
+    maxNumber = arrayNumbers[i]
+  }
+}
+
+console.log(maxNumber);
+
+let maximo = Math.max(12, 28, 1879, 4, 29);
+
+console.log(maximo);
